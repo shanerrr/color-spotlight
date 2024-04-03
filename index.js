9108851102dff21989fc643018caa0b861a0b688
Shane Goonasekera <theshanukg@gmail.com>
2024-04-03 00:33:26 -0600

    optimzed codebase

--- index.js
+++ index.js
@@ -1,44 +1,34 @@
 const { createCanvas, loadImage } = require("canvas");
 
-function componentToHex(c) {
-  let hex = parseInt(c).toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
+const componentToHex = (c) => {
+  const hex = parseInt(c, 10).toString(16);
+  return hex.padStart(2, "0");
+};
 
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
+const rgbToHex = (r, g, b) =>
+  `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`;
 
-async function getMostProminentColor(imageUrl) {
+const getMostProminentColor = async (imageUrl) => {
   try {
-    // Load the image
     const image = await loadImage(imageUrl);
-
-    // Create a canvas and draw the image on it
     const canvas = createCanvas(image.width, image.height);
     const ctx = canvas.getContext("2d");
-    ctx.drawImage(image, 0, 0, image.width, image.height);
+    ctx.drawImage(image, 0, 0);
 
-    // Get pixel data
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-
-    // Calculate the frequency of each color
+    const { data } = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const colorCount = {};
-    for (let i = 0; i < imageData.length; i += 4) {
-      const r = imageData[i];
-      const g = imageData[i + 1];
-      const b = imageData[i + 2];
-      const rgb = `${r},${g},${b}`;
+    for (let i = 0; i < data.length; i += 4) {
+      const rgb = `${data[i]},${data[i + 1]},${data[i + 2]}`;
       colorCount[rgb] = (colorCount[rgb] || 0) + 1;
     }
 
-    // Find the most frequent color
     let maxCount = 0;
     let mostProminentColor = {};
-    for (const color in colorCount) {
-      if (colorCount[color] > maxCount) {
-        maxCount = colorCount[color];
-        mostProminentColor = { rgb: color, hex: rgbToHex(...color.split(",")) };
+    for (const [rgb, count] of Object.entries(colorCount)) {
+      if (count > maxCount) {
+        maxCount = count;
+        const [r, g, b] = rgb.split(",").map(Number);
+        mostProminentColor = { rgb, hex: rgbToHex(r, g, b) };
       }
     }
 
@@ -46,6 +36,6 @@ async function getMostProminentColor(imageUrl) {
   } catch (error) {
     throw new Error("Error processing image: " + error.message);
   }
-}
+};
 
 module.exports = getMostProminentColor;
